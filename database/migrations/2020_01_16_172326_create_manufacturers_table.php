c2178e75b16d2c08f36a53e95e2bc0198f2b1297
Radostin Todorov <rado1998@abv.bg>
2020-01-16 20:11:55 +0200

    Added manufacturers model and controller. Added index view for the manufacturers controller. Changes to routes.

--- database/migrations/2020_01_16_172326_create_manufacturers_table.php
+++ database/migrations/2020_01_16_172326_create_manufacturers_table.php
@@ -14,7 +14,8 @@ class CreateManufacturersTable extends Migration
     public function up()
     {
         Schema::create('manufacturers', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
+            $table->string('name', 20);
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix' => 'admin', 'middleware' => ['web', 'auth', 'admin'], 'namespace' => 'Admin'], function () {
+    Route::resource('manufacturers', 'ManufacturersController');
+});
