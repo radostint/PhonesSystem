ee94a885ec7340453a530d3909ada3674d7699ef
Radostin Todorov <rado1998@abv.bg>
2020-02-02 11:02:32 +0200

    Added db seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(ManufacturersTableSeeder::class);
+        $this->call(PhonesTableSeeder::class);
     }
 }
